4514707e4f09d6949953d4bcef6519a040a90abf
Marco Gutama <mgutama@baustro.fin.ec>
2025-07-04 16:46:15 -0500

    refactor(controller): eliminar código inseguro
    Se eliminó código vulnerable a XSS y que expone
    el stacktrace.  Se simplificó el controlador.

--- demo/src/main/java/com/demo/controllers/WelcomeController.java
+++ demo/src/main/java/com/demo/controllers/WelcomeController.java
@@ -1,17 +1,5 @@
 package com.demo.controllers;
 
-import com.demo.models.WelcomeDTO;
-import org.springframework.web.bind.annotation.*;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import java.io.*;
-import java.nio.file.*;
-import java.security.MessageDigest;
-import java.sql.*;
-import java.util.Base64;
 import java.util.logging.Logger;
 
 @RestController
@@ -24,20 +12,4 @@ public class WelcomeController {
     private static final String DB_PASSWORD = "SuperSecreta123!";
     private static final String API_KEY = "ABC123-TOKEN-INSEGURO";
 
-    // XSS
-    @GetMapping("/api/welcome")
-    public WelcomeDTO welcome(@RequestParam(value = "name", defaultValue = "...") String name) {
-        return new WelcomeDTO("Hola, bienvenido " + name + ", esto es un demo");
-    }
-
-    // Stacktrace expuesto
-    @GetMapping("/api/error")
-    public String error() {
-        try {
-            int a = 1 / 0;
-            return "OK";
-        } catch (Exception e) {
-            return e.toString(); // ⚠ Devuelve detalles internos
-        }
-    }
 }
