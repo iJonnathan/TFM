fed052e5683c69dc4e43286230f4c4cdbce1082b
ijonna <ijonna@ijonnas-MacBook-Pro.local>
2025-08-24 11:33:52 -0500

    solve one vulnerability

--- demo/src/main/java/com/demo/controllers/WelcomeController.java
+++ demo/src/main/java/com/demo/controllers/WelcomeController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
+import org.springframework.http.ResponseEntity;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
