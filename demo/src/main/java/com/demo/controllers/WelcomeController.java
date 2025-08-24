632c226915863134f3bb3e97d86f5d505f846b8b
ijonna <ijonna@ijonnas-MacBook-Pro.local>
2025-08-24 11:28:33 -0500

    solve one vulnerability

--- demo/src/main/java/com/demo/controllers/WelcomeController.java
+++ demo/src/main/java/com/demo/controllers/WelcomeController.java
@@ -28,15 +28,27 @@ import java.util.logging.Logger;
 @RestController
 public class WelcomeController {
 
-    private static final Logger logger = Logger.getLogger(WelcomeController.class.getName());
-    private static final int GCM_TAG_LENGTH = 128;
+    private static final String BASE_DIRECTORY = "/var/logs/app/";
 
 
     @GetMapping("/api/welcome")
     public WelcomeDTO welcome(@RequestParam(value = "name", defaultValue = "...") String name) {
         return new WelcomeDTO("Hola, bienvenido " +name + ", esto es un demo");
     }
-    
+
+     @GetMapping("/logs")
+    public ResponseEntity<String> getLogFile(@RequestParam String filename) {
+        try {
+            
+            Path filePath = Paths.get(BASE_DIRECTORY + filename);
+
+            String content = new String(Files.readAllBytes(filePath));
+            return ResponseEntity.ok(content);
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: Archivo no encontrado.");
+        }
+    }
 
 
 }
