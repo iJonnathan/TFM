c3ba94362012ec93faae4694427429341ee1bac0
ijonna <ijonna@ijonnas-MacBook-Pro.local>
2025-09-09 23:09:32 -0500

    solve vulnerability

--- demo/src/main/java/com/demo/controllers/WelcomeController.java
+++ demo/src/main/java/com/demo/controllers/WelcomeController.java
@@ -24,7 +24,7 @@ import java.sql.ResultSet;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import java.util.*;
 @RestController
 public class WelcomeController {
 
